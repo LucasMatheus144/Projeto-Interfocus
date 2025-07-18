4a532f7bf25e1bac1c74f2145c736a06f4df93c1
Lucas Marques <lucasmatheussouza53@gmail.com>
2025-07-18 11:13:50 -0300

    adicionando comentarios

--- VendinhaReact/src/App.jsx
+++ VendinhaReact/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
+import Loading from './Components/loading/loading';
 
 //https://legacy-reactjs-org.translate.goog/docs/code-splitting.html?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt&_x_tr_pto=tc&_x_tr_hist=true
+// evitar de renderizar componente ser ser chamado.
 const NotFound = lazy(() => import('./Pages/Error/index'));
 const HomePage = lazy(() => import('./Pages/Home/index'));
 const Dividas = lazy(() => import('./Pages/Dividas/index'));
 const Layout = lazy(() => import('./Components/Layout/compartilhado'));
 const Relatorio = lazy(() => import('./Pages/Relatorio/relatorio'));
-const Loading = lazy(() => import('./Components/loading/loading') )
 
 export default function App() {
   return (
