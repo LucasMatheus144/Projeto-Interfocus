9e941f69c9f4ad34356941f074763b7a54109f93
Lucas Matheus <lucasmatheussouza53@gmail.com>
2025-07-19 00:19:38 -0300

    Vendo a diferena de otimizacao em prod

--- VendinhaReact/src/App.jsx
+++ VendinhaReact/src/App.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import Loading from './Components/loading/loading';
+import HomePage from './Pages/Home/index';
+import NotFound from './Pages/Error/index';
+import Dividas from './Components/FormDivida/Divida';
+import Layout from './Components/Layout/compartilhado';
+import Relatorio from './Pages/Relatorio/relatorio';
 
 //https://legacy-reactjs-org.translate.goog/docs/code-splitting.html?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt&_x_tr_pto=tc&_x_tr_hist=true
 // evitar de renderizar componente ser ser chamado.
-const NotFound = lazy(() => import('./Pages/Error/index'));
-const HomePage = lazy(() => import('./Pages/Home/index'));
-const Dividas = lazy(() => import('./Pages/Dividas/index'));
-const Layout = lazy(() => import('./Components/Layout/compartilhado'));
-const Relatorio = lazy(() => import('./Pages/Relatorio/relatorio'));
+// const NotFound = lazy(() => import('./Pages/Error/index'));
+// const Dividas = lazy(() => import('./Pages/Dividas/index'));
+// const Layout = lazy(() => import('./Components/Layout/compartilhado'));
+// const Relatorio = lazy(() => import('./Pages/Relatorio/relatorio'));
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loading/>}>
+      {/* <Suspense fallback={<Loading/>}> */}
         <Routes>
           {/* Layout persistente */}
           <Route path="/" element={<Layout />}>
@@ -25,7 +29,7 @@ export default function App() {
           {/* 404 sem layout */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </Suspense>
+      {/* </Suspense> */}
     </BrowserRouter>
   );
 }
